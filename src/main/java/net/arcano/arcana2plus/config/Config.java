97a5a8761c2ca5b091383c7a12aa9200a18c130c
Arcano <ren.cunha@hotmail.com>
2022-02-28 12:07:57 -0300

    Generate config file

---
+++ src/main/java/net/arcano/arcana2plus/config/Config.java
@@ -0,0 +1,8 @@
+package net.arcano.arcana2plus.config;
+
+public class Config {
+    public boolean genUndergroundClay = true;
+    public boolean useCoalVariants = true;
+    public boolean allTreesDropFruits = true;
+    public boolean useMoreShears = true;
+}
