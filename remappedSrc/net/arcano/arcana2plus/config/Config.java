56321956b869039c0fbcb62a6fe3305902069d90
Arcano <ren.cunha@hotmail.com>
2022-03-15 11:11:48 -0300

    Version Update + Config File

--- remappedSrc/net/arcano/arcana2plus/config/Config.java
+++
@@ -1,8 +0,0 @@
-package net.arcano.arcana2plus.config;
-
-public class Config {
-    public boolean genUndergroundClay = true;
-    public boolean useCoalVariants = true;
-    public boolean allTreesDropFruits = true;
-    public boolean useMoreShears = true;
-}
